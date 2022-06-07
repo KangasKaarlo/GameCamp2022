7db58eb0b27d5c4fbb24c4596f39af3d268fdf3f
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2022-06-07 21:18:12 +0300

    Add UI for choosing the tower to place and Add placeholders for all three towers

--- GameCamp2022/Assets/Scripts/TowerPlacement.cs
+++ GameCamp2022/Assets/Scripts/TowerPlacement.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerPlacement : MonoBehaviour
 {
     public GameObject towerNormal;
+    public GameObject towerFast;
+    public GameObject towerBrute;
+    string activeButton = "NormalTowerButton";
+    public Button normalTowerButton;
+    public Button fastTowerButton;
+    public Button bruteTowerButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Finds UI buttons and adds onclick functions to them
+        normalTowerButton = GameObject.Find("NormalTowerButton").GetComponent<Button>();
+        normalTowerButton.onClick.AddListener(selectNormalTower);
+
+        fastTowerButton = GameObject.Find("FastTowerButton").GetComponent<Button>();
+        fastTowerButton.onClick.AddListener(selectFastTower);
+
+        bruteTowerButton = GameObject.Find("BruteTowerButton").GetComponent<Button>();
+        bruteTowerButton.onClick.AddListener(selectBruteTower);
     }
 
     // Update is called once per frame
@@ -20,6 +35,30 @@ public class TowerPlacement : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Instantiate(towerNormal, this.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        if(activeButton == "NormalTowerButton")
+        {
+            Instantiate(towerNormal, this.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        } else if (activeButton == "FastTowerButton")
+        {
+            Instantiate(towerFast, this.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+        else if (activeButton == "BruteTowerButton")
+        {
+            Instantiate(towerBrute, this.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+
+
+    }
+    public void selectNormalTower()
+    {
+        activeButton = "NormalTowerButton";
+    }
+    public void selectFastTower()
+    {
+        activeButton = "FastTowerButton";
+    }
+    public void selectBruteTower()
+    {
+        activeButton = "BruteTowerButton";
     }
 }
