0fd022694901343316b02068263ab89907f42356
VilleKarilainen <ville.karilainen@gmail.com>
2022-06-09 14:39:27 +0300

    add pathfinding to test enemy

--- GameCamp2022/Assets/TestAssets/TestEnemy.cs
+++ GameCamp2022/Assets/TestAssets/TestEnemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class TestEnemy : MonoBehaviour
 {
+    public float speed;
+    private WayPoints Wpoints;
+    private int waypointIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<WayPoints>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += new Vector3(5 * Time.deltaTime,0,0); 
+        //this.transform.position += new Vector3(5 * Time.deltaTime,0,0); 
+        transform.position = Vector3.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, Wpoints.waypoints[waypointIndex].position)< 0.1f)
+        {
+            
+            if (waypointIndex < Wpoints.waypoints.Length - 1)
+            {
+                waypointIndex++;
+            }else{
+                Debug.Log("End Reached");
+            }
+        }
+
+
     }
 }
